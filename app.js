276324a49e261ef9a760d4fdb18a9421b6d62c79
Yuchuan <yyc478@gmail.com>
2016-10-13 17:40:10 +0800

    Move routes folder to /app

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./app/routes/index');
 
 var app = express();
 
 var db = require('./app/models');
-
 var globals = {
   db
 };
@@ -29,7 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes(globals));
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var auth = require('./auth');
+
 module.exports = function(globals){
   /* GET home page. */
   router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
   });
+  router.use('/auth/',auth(globals));
 
   return router;
 }
