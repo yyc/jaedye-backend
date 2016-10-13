4bdea803877ef694446deabdf102d9c9174ee5bf
Yuchuan <yyc478@gmail.com>
2016-10-13 19:09:52 +0800

    Add facebook auth strategy

---
+++ config/facebook.js
@@ -0,0 +1,29 @@
+var secrets = require('../config/secrets.js');
+var FacebookStrategy = require('passport-facebook-token');
+
+module.exports = function(db){
+  return new FacebookStrategy({
+    clientID: secrets.facebook.clientID,
+    clientSecret: secrets.facebook.clientSecret,
+    callbackURL: secrets.url + '/auth/facebook/',
+    profileFields: ['id', 'displayName', 'photos', 'email']
+  }, function(accessToken, refreshToken, profile, cb){
+    db.User.findOrCreate({
+        where: {
+          provider: 'facebook',
+          providerId: profile.id
+        },
+        defaults: {
+          email: profile.email
+        }
+      })
+    .spread(function(user, wasCreated){
+      user.set('providerToken', accessToken);
+      user.set('name', profile.displayName);
+      return user.save();
+    })
+    .then(function(user){
+      cb(null, user);
+    });
+  });
+}
