cc1be4252f46560f3fa95f512fee48fa9bfb2e47
Yuchuan <yyc478@gmail.com>
2016-10-18 14:24:35 +0800

    Add picture field to user model

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,7 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
+    picture: DataTypes.STRING,
     provider: DataTypes.STRING,
     providerId: DataTypes.STRING,
     providerToken: DataTypes.STRING
--- db/migrate/20161003130846-create-user.js
+++ db/migrate/20161003130846-create-user.js
@@ -23,6 +23,9 @@ module.exports = {
       providerToken: {
         type: Sequelize.STRING
       },
+      picture: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
