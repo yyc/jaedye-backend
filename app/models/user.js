a0bde0b9b51602045e01a50e993e9d5959fe8968
Yuchuan <yyc478@gmail.com>
2016-10-18 14:32:07 +0800

    Add associations to user and attempts

--- app/models/user.js
+++ app/models/user.js
@@ -10,7 +10,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+        this.hasMany(models.Attempt);
       }
     }
   });
