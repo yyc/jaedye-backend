cfe66e1472685fb2aaabfc18e8cc3b61e3805f93
Yuchuan <yyc478@gmail.com>
2016-10-18 15:00:01 +0800

    Add attempts fetch route

--- app/routes/attempts.js
+++ app/routes/attempts.js
@@ -2,5 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function(globals){
+  var db = globals.db;
+  router.get('/', function(req, res, next){
+    req.user.model
+    .then((user) => user.getAttempts({limit: req.query.limit, offset: req.query.offset}))
+    .then(function(attempts){
+      res.json(attempts.map((attempt) => attempt.getDataValues(), attempts));
+    });
+  });
   return router;
 }
