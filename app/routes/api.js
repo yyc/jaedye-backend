8549ab2832ad862f7db23f6984bcc4f0753ac8fc
Yuchuan <yyc478@gmail.com>
2016-10-18 14:32:27 +0800

    Fix routing for attempt endpoints

--- app/routes/api.js
+++ app/routes/api.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 var attempts = require('./attempts');
 
+
 module.exports = function(globals){
-  router.use(attempts(globals));
+  router.use('/attempts', attempts(globals));
   return router;
 }
