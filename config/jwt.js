5a1dddb67e3bfd845f0c3cd41baff3069c233d9d
Yuchuan <yyc478@gmail.com>
2016-10-18 15:11:08 +0800

    Fix LazyUser in jwt

--- config/jwt.js
+++ config/jwt.js
@@ -5,7 +5,28 @@ var passportJwt = require('passport-jwt');
   var JwtStrategy = passportJwt.Strategy;
   var jwtExtractors = passportJwt.ExtractJwt;
 
-module.exports = function(db){
+module.exports = function(globals){
+  var db = globals.db;
+  // Optimization for JWT, so we only hit the DB for the actual user model when we need it
+  class LazyUser {
+    constructor(userJson){
+      Object.assign(this, userJson);
+      this.__model = undefined;
+    }
+    get model(){
+      if(this.__model){
+        return Promise.resolve(this.__model);
+      }
+      else {
+        var self = this;
+        return db.User.findById(this.id).then(function(user){
+          this.__model = user;
+          return user;
+        });
+      }
+    }
+  }
+
   var strategy = new JwtStrategy({
       secretOrKey: secrets.jwt.publicKey,
       jwtFromRequest: jwtExtractors.fromHeader('access-token'),
@@ -18,21 +39,5 @@ module.exports = function(db){
   return {
     strategy,
     jsonwebtoken
-  }
-}
-
-// Optimization for JWT, so we only hit the DB for the actual user model when we need it
-class LazyUser {
-  constructor(userJson){
-    Object.assign(this, userJson);
-    this.__model = undefined;
-  }
-  get model(){
-    if(this.__model){
-      return Promise.resolve(this.__model);
-    }
-    else {
-      return db.User.findById(this.id);
-    }
   }
 }
