ad193c6728abd177d3b77fcd1bd5026821794a80
Yuchuan <yyc478@gmail.com>
2016-10-24 00:41:37 +0800

    Add friends routes

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var attempts = require('./attempts');
-
+var friends = require('./friends');
 
 module.exports = function(globals){
   router.use('/attempts', attempts(globals));
+  router.use('/friends', friends(globals));
   return router;
 }
---
+++ app/routes/friends.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = function(globals){
+  var db = globals.db;
+  router.get('/', function(req, res, next){
+    req.user.model
+    .then((user) => user.getFriends())
+    .then(function(friends){
+      res.json(friends.map((friend) => friend.dataValues));
+    });
+  });
+  router.post('/', function(req, res, next){
+    if(typeof req.body != 'number'){
+      res.status(400);
+      res.json({error: 'Post body is not a user ID'});
+      return;
+    }
+    db.User.findById({id: req.body})
+    .then(function(user){
+      if(user == null){
+        throw 'User not found!';
+      }
+      return db.Friendship.findOrCreate({
+        where: { UserId: req.user.id },
+        defaults: { friendTo: req.body }
+      });
+    })
+    .spread(function(newFriendship, created){
+      if(!created){
+        throw 'User has already been added as friend!'
+      }
+      return db.Friendship.findOne({
+        where: {friendTo: req.user.id,
+                UserId: req.body}
+      }).then(function(cFriendship){
+        if(cFriendship == null){
+          res.json({message: 'Friend request sent!'});
+        } else{
+          return Promise.all([
+          newFriendship.update({isPending: false}),
+          cFriendship.update({isPending: true})])
+        }
+      });
+    })
+    .spread(function(newFriendship, cFriendship){
+      return cFriendship.getUser();
+    })
+    .then(function(user){
+      res.json({
+        message: "Friend added!",
+        user: user.dataValues
+      });
+    }).catch(function(error){
+      res.status(400);
+      res.json({error});
+    })
+    ;
+  });
+  return router;
+}
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
