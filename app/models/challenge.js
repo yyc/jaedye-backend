c57ddd7a6e9a01e29754983f4e5fcc6b3dbe64db
Yuchuan <yyc478@gmail.com>
2016-10-23 21:35:43 +0800

    Add challenge model and migration

---
+++ app/models/challenge.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Challenge = sequelize.define('Challenge', {
+    name: DataTypes.STRING,
+    startDate: DataTypes.DATE,
+    duration: DataTypes.INTEGER,
+    description: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        this.hasMany(models.ChallengeUser);
+        this.belongsToMany(models.User,{as: 'challengers', through:models.ChallengeUser});
+      }
+    }
+  });
+  return Challenge;
+};
---
+++ db/migrate/20161022021049-create-challenge.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Challenges', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      startDate: {
+        type: Sequelize.DATE
+      },
+      duration: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Challenges');
+  }
+};
