ae5bccd316af17a07a3e6c85ab77fcb652794b6a
Yuchuan <yyc478@gmail.com>
2016-10-25 22:32:20 +0800

    Add challenges routes

--- app/routes/challenges.js
+++ app/routes/challenges.js
@@ -11,7 +11,7 @@ module.exports = function(globals){
         UserId: req.user.id
       },
       order: ['isPending'],
-      include:['Challenge', 'Inviter']
+      include:[db.Challenge, 'Inviter']
     })
     req.user.model
     .then((user) => user.getChallenges({isPending: false}))
@@ -20,52 +20,46 @@ module.exports = function(globals){
     });
   });
 
+  // POST /api/challenges  -- create new challenge
   router.post('/', function(req, res, next){
-    if(typeof req.body != 'number'){
-      res.status(400);
-      res.json({error: 'Post body is not a user ID'});
-      return;
+    if(['name', 'startDate', 'endDate','people']
+      .reduce((result, ind) => result && (req.body[ind] != undefined)), true){
+        // One or more fields are missing
+        res.status(400);
+        res.json({error: 'Missing field in New Challenge object'});
+        return;
     }
-    db.User.findById({id: req.body})
+    req.user.model
     .then(function(user){
-      if(user == null){
-        throw 'User not found!';
-      }
-      return db.Friendship.findOrCreate({
-        where: { UserId: req.user.id },
-        defaults: { friendTo: req.body }
+      return new Promise(function(resolve, reject){
+          globals.fb.api(`me/friends`, {access_token: user.providerToken}, function(fbResponse){
+            var friendsList = fbResponse.data.map((friend) => friend["id"]);
+            resolve(friendsList);
+          });
       });
     })
-    .spread(function(newFriendship, created){
-      if(!created){
-        throw 'User has already been added as friend!'
-      }
-      return db.Friendship.findOne({
-        where: {friendTo: req.user.id,
-                UserId: req.body}
-      }).then(function(cFriendship){
-        if(cFriendship == null){
-          res.json({message: 'Friend request sent!'});
-        } else{
-          return Promise.all([
-          newFriendship.update({isPending: false}),
-          cFriendship.update({isPending: true})])
-        }
+    .then(function(friendsList){
+      return db.User.findAll({
+        provider: 'facebook',
+        providerId: friendsList
+      })
+    })
+    .then(function(userList){
+      var users = userList.filter((user) => req.body.people.includes(user.id));
+      return db.Challenge.build({
+        name: req.body.name,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        challengers: users
       });
     })
-    .spread(function(newFriendship, cFriendship){
-      return cFriendship.getUser();
+    .then(function(challenge){
+      res.json(challenge);
     })
-    .then(function(user){
-      res.json({
-        message: "Friend added!",
-        user: user.dataValues
-      });
-    }).catch(function(error){
+    .catch(function(error){
       res.status(400);
       res.json({error});
-    })
-    ;
+    });
   });
   return router;
 }
