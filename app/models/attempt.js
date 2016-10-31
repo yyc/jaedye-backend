13b998c17cfec45adb03ecfd47289de9ee8af53a
Yuchuan <yyc478@gmail.com>
2016-10-31 14:31:58 +0800

    Only update challenges if the user is successful

--- app/models/attempt.js
+++ app/models/attempt.js
@@ -16,21 +16,24 @@ module.exports = function(sequelize, DataTypes) {
     return new Date(this.getDataValue("startTime").getTime() + this.getDataValue("actualTime")*60000);
   }
   Attempt.afterCreate(function(attempt, options){
-    attempt.getUser()
-    .then(function(user){
-      return user.getActiveChallengesForAttempt(attempt)
-    })
-    .then(function(challengeUsers){
-      // For each challenge, add the overlapping time (in minutes);
-      challengeUsers.forEach(function(cu){
-        var time = attempt.actualTime * 60000;
-        time -= Math.max(cu.Challenge.startDate - attempt.startTime, 0);
-        time -= Math.max(attempt.getEndTime() - cu.Challenge.endDate, 0);
-        time = Math.round(time / 60000);
-        cu.setDataValue("time", time);
-        cu.save();
+    // Only update challenges if successful
+    if(attempt.actualTime >= attempt.targetTime){
+      attempt.getUser()
+      .then(function(user){
+        return user.getActiveChallengesForAttempt(attempt)
       })
-    })
+      .then(function(challengeUsers){
+        // For each challenge, add the overlapping time (in minutes);
+        challengeUsers.forEach(function(cu){
+          var time = attempt.actualTime * 60000;
+          time -= Math.max(cu.Challenge.startDate - attempt.startTime, 0);
+          time -= Math.max(attempt.getEndTime() - cu.Challenge.endDate, 0);
+          time = Math.round(time / 60000);
+          cu.setDataValue("time", time);
+          cu.save();
+        });
+      })
+    }
   })
   return Attempt;
 };
