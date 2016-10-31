3d2c2429b17b44e8c91217838b36f907e0f56d29
Yuchuan <yyc478@gmail.com>
2016-10-31 14:31:37 +0800

    Add attempts model hook that updates the user's challenges

--- app/models/attempt.js
+++ app/models/attempt.js
@@ -15,5 +15,22 @@ module.exports = function(sequelize, DataTypes) {
   Attempt.Instance.prototype.getEndTime = function(){
     return new Date(this.getDataValue("startTime").getTime() + this.getDataValue("actualTime")*60000);
   }
+  Attempt.afterCreate(function(attempt, options){
+    attempt.getUser()
+    .then(function(user){
+      return user.getActiveChallengesForAttempt(attempt)
+    })
+    .then(function(challengeUsers){
+      // For each challenge, add the overlapping time (in minutes);
+      challengeUsers.forEach(function(cu){
+        var time = attempt.actualTime * 60000;
+        time -= Math.max(cu.Challenge.startDate - attempt.startTime, 0);
+        time -= Math.max(attempt.getEndTime() - cu.Challenge.endDate, 0);
+        time = Math.round(time / 60000);
+        cu.setDataValue("time", time);
+        cu.save();
+      })
+    })
+  })
   return Attempt;
 };
