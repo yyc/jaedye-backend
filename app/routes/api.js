69b905751d67b0de19907918312ca409a3520c52
Yuchuan <yyc478@gmail.com>
2016-11-03 23:25:00 +0800

    Add /api/image endpoint

--- app/routes/api.js
+++ app/routes/api.js
@@ -9,5 +9,8 @@ module.exports = function(globals){
   router.use('/attempts', attempts(globals));
   router.use('/friends', friends(globals));
   router.use('/challenges', challenges(globals));
+  router.get('/image', function(req, res, next){
+    res.end(process.env.IMAGE_URL || "https://jaedye.herokuapp.com/images/default.jpg");
+  });
   return router;
 }
