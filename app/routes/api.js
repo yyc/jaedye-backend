324ff3b0d9d3ee5791d9fb0163d48fb230ce35dc
Yuchuan <yyc478@gmail.com>
2016-11-04 16:43:30 +0800

    Make image endpoint return a json object with link and metadata

--- app/routes/api.js
+++ app/routes/api.js
@@ -10,7 +10,11 @@ module.exports = function(globals){
   router.use('/friends', friends(globals));
   router.use('/challenges', challenges(globals));
   router.get('/image', function(req, res, next){
-    res.end(process.env.IMAGE_URL || "https://jaedye.herokuapp.com/images/default.jpg");
+    res.json({
+      image_url: process.env.IMAGE_URL || "https://jaedye.herokuapp.com/images/default.jpg",
+      source: process.env.IMAGE_SOURCE || "Jaedye Team",
+      source_url: process.env.IMAGE_SOURCE_URL || "https://jaedye.herokuapp.com"
+    });
   });
   return router;
 }
